3ba803203c02b3c65ee26b564b380d05ecec417c
hehepeng <18846042826@163.com>
2021-12-20 14:41:13 +0800

    完成第三方接口请求的封装

---
+++ src/main/java/com/example/schedule/ScheduleExample.java
@@ -0,0 +1,37 @@
+package com.example.schedule;
+
+import com.example.pojo.User;
+import com.example.service.UserService;
+import com.example.util.HttpClientComponent;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+@DisallowConcurrentExecution
+public class ScheduleExample {
+
+    @Autowired
+    private UserService userService;
+
+    @Scheduled(cron = "0/10 * * * * ?")  //每10秒执行一次
+    public void excTask(){
+        System.out.println("定时任务执行，执行时间是："+new Date());
+        List<User> users = userService.getUsers();
+        System.out.println("查询数据库user表的全部值是:"+users);
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("X-Shopify-Access-Token", "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");
+
+        HttpClientComponent.doGet("https://amelie-home-shop.myshopify.com/admin/api/2021-10/customers.json", "UTF-8", map);
+
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("commentId", "13026194071");
+//        doPost("http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=13026194071", jsonObject);
+    }
+}
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -3,14 +3,18 @@ package com.example.service;
 import com.example.pojo.Role;
 import com.example.pojo.User;
 
+import java.util.List;
 import java.util.Map;
 
 public interface UserService {
     // 用户注册
     public int userRegister(Map<String, Object> userInfo);
 
-    // 查询用户
+    // 根据用户名查询用户
     public User getUser(User user);
+
+    // 获取所有用户
+    public List<User> getUsers();
 
     // 根据userId查询某个用户
     public User getUserById(int userId);
